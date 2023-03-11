90f12c981997923a49dfd4d45eb54d0fbeab205d
LeonLongZeyu <62172956+LeonLongZeyu@users.noreply.github.com>
2023-03-11 15:54:51 -0500

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -39,117 +39,118 @@ int execute_command(struct pipeline* the_pipeline, int input, int first, int las
     int status;
     int file_descriptor[2];
 
-    if (pipe(file_descriptor) < 0)
-    {
-        perror("ERROR: Failed to create pipe.\n");
-        exit(1);
-    }
-
+    //Forking
     child_pid = fork();
-    if (child_pid < 0)
-    {
-        perror("ERROR: Failed to fork.\n");
-        exit(1);
-    }
 
+    //Creating a pipe
+    pipe(file_descriptor);
+    
     //Child
     if (child_pid == 0)
     {
-        close(file_descriptor[0]);
 
-        if (background)
+
+
+        if(background)
         {
-            close(0);
-            dup(input);
-            close(input);
+            close(file_descriptor[1]);
+        }
+
+        if (the_pipeline -> commands -> redirect_out_path)
+        {
+            int fd_out = creat(the_pipeline -> commands -> redirect_out_path, 0644);
+
+            if (fd_out == -1)
+            {
+                perror("ERROR: Failed to open output file.\n");
+                exit(0);
+            }
+            
+            file_descriptor[1] = fd_out;
+        }
+
+        if (the_pipeline -> commands -> redirect_in_path)
+        {
+            int input_fd = open(the_pipeline -> commands -> redirect_in_path, 0x0000);
+
+            if (input_fd < 0)
+            {
+                perror("ERROR: Failed to open input file.\n");
+                exit(0);
+            }
+
+            if (dup2(input_fd, 0) < 0)
+            {
+                perror("ERROR: Failed to redirect input.\n");
+                exit(0);
+            }
+            close(input_fd);
+
+            file_descriptor[0] = input_fd;
+        }
+        
+        if(first == 1 && last == 0 && input == 0)
+        {
+            dup2(file_descriptor[1], 1);
+        }
+        else if(first == 0 && last == 0 && input != 0)
+        {
+            dup2(input, 0);
+            dup2(file_descriptor[1], 1);
         }
         else
         {
-            if (input != 0)
+            if(the_pipeline -> commands -> redirect_out_path)
             {
-                dup2(input, 0);
-                close(input);
+                dup2(file_descriptor[1], 1);
             }
+
+            if(the_pipeline -> commands -> redirect_in_path)
+            {
+                dup2(file_descriptor[1], 0);
+            }
+            dup2(input, 0);
         }
 
-        if (last == 1)
+        //for fork()
+        if(execvp(the_pipeline -> commands -> command_args[0], the_pipeline -> commands -> command_args) == -1)
         {
-            if (the_pipeline->commands->redirect_out_path)
-            {
-                int fd_out = creat(the_pipeline->commands->redirect_out_path, 0644);
-
-                if (fd_out < 0)
-                {
-                    perror("ERROR: Failed to create output file.\n");
-                    exit(1);
-                }
-
-                dup2(fd_out, STDOUT_FILENO);
-                close(fd_out);
-            }
-            else
-            {
-                dup2(file_descriptor[1], STDOUT_FILENO);
-            }
-        }
-        else if (first == 1)
-        {
-            if (the_pipeline->commands->redirect_in_path)
-            {
-                int fd_in = open(the_pipeline->commands->redirect_in_path, O_RDONLY);
-
-                if (fd_in < 0)
-                {
-                    perror("ERROR: Failed to open input file.\n");
-                    exit(1);
-                }
-
-                dup2(fd_in, STDIN_FILENO);
-                close(fd_in);
-            }
-            else
-            {
-                dup2(file_descriptor[1], STDOUT_FILENO);
-            }
-        }
-        else
-        {
-            dup2(input, STDIN_FILENO);
-            dup2(file_descriptor[1], STDOUT_FILENO);
-        }
-
-        if (execvp(the_pipeline->commands->command_args[0], the_pipeline->commands->command_args) == -1)
-        {
-            perror("ERROR: Failed to execute command.\n");
+            perror("ERROR: Failed to fork.\n");
             exit(1);
         }
 
+        close(file_descriptor[0]);
         close(file_descriptor[1]);
+        close(0);
+        dup(0);        
     }
     //Parent
     else
     {
-        if (!background)
+        //Waiting for the child processes not in background
+        if(!background)
         {
-            waitpid(child_pid, &status, 0);
+            wait(&status);
         }
 
+        //Closing the write end of the pipe
         close(file_descriptor[1]);
     }
-
-    if (input != 0)
+    
+    //Closing the input
+    if(input != 0)
     {
         close(input);
     }
 
-    if (last == 1)
+    //Closing the read end of the pipe
+    if(last == 1)
     {
-        return file_descriptor[0];
+        close(file_descriptor[0]);
     }
-    else
-    {
-        return file_descriptor[1];
-    }
+    
+    //Returning the read end to the next pipe as an input
+    return file_descriptor[0];
 }
 
 
